c176024112b422a60a2d8d1bc3000e8a2cad6708
ali3dashti <aadashti73@gmail.com>
2020-06-15 00:18:12 +0300

    تم حل الواجب

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,7 +7,7 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = myWeight + Int(FriendWeight)
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,28 +38,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "ali"
+var lastName1 = "dashti"
+var email1 = "123abc@gmail.com"
+var phoneNumber1 = 12345678
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "jabriya"
+var password = "hui"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
-
+var firstName2 = "mohammed"
+var lastName2 = "dashti"
+var email2 = "4456def@hotmail.com"
+var phoneNumber2 = 87654321
+var age2 = 35
+var country2 = "uae"
+var area2 = "surra"
+var password2 = "jkl"
+var isKuwaiti2 = false
 
 
 
 //Person 3
+var firstName3 = "yousef"
+var lastName3 = "alali"
+var email3 = "yo12@yahoo.com"
+var phoneNumber3 = 12365409
+var age3 = 20
+var country3 = "kuwait"
+var area3 = "jabriya"
+var password3 = "ykhfsd"
+var isKuwaiti3 = true
+
+
 
 
 
@@ -82,3 +95,32 @@ var isKuwaiti = false
 
  
  */
+age1 == age2
+age1 != age2
+age1 == age3
+age1 != age3
+age2 == age3
+age2 != age3
+
+area1 == area2
+area1 != area2
+area1 == area3
+area1 != area3
+area2 == area3
+area2 != area3
+
+age1 > 18
+age2 > 18
+age3 > 18
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
